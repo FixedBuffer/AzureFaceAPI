b8e704196ae814f498e8caf9773415b9732713b4
JorTurFer <jorge_turrado@hotmail.es>
2019-01-11 20:23:33 +0100

    Añado ConfigurationManager y sus propiedades para usar

--- PostAzureFaceAPI/MainForm.cs
+++ PostAzureFaceAPI/MainForm.cs
@@ -6,15 +6,19 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace PostAzureFaceAPI
 {
     public partial class MainForm : Form
     {
+        string FaceAPIKey = ConfigurationManager.AppSettings["FaceAPIKey"];
+        string FaceAPIEndPoint = ConfigurationManager.AppSettings["FaceAPIEndPoint"];
+
         public MainForm()
-        {
-            InitializeComponent();
+        {           
+            InitializeComponent();           
         }
     }
 }
